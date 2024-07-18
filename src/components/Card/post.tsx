e0ea06695d44eec3dafd92195a1df3c1c052c0a8
Yerovy Espitia <luyeesgar@gmail.com>
2024-07-18 14:28:26 -0500

    feat: add PostCard comp

---
+++ src/components/Card/post.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import { Tags } from './tags'
+import Image from 'next/image'
+
+export const PostCard = () => {
+  return (
+    <article className='border rounded-lg border-custom-light-black hover:bg-custom-gray'>
+      <Link href={'/game'}>
+        <div className='flex flex-col p-3'>
+          <article className='flex flex-col gap-3'>
+            <h2 className='text-lg font-semibold text-gray-400 capitalize'>
+              The last of us
+            </h2>
+
+            <p className='text-gray-200'>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus
+              consequuntur delectus iste quia eligendi excepturi? Qui,
+              doloribus. Lorem ipsum dolor sit, amet consectetur adipisicing
+              elit. Minus consequuntur delectus iste quia eligendi excepturi?
+              Qui, doloribus.
+            </p>
+
+            <footer className='flex flex-col justify-between sm:flex-row'>
+              <div className='flex items-center gap-2 mb-3 sm:mb-0'>
+                <Image
+                  className='rounded-full size-5'
+                  src={'/imgs/square.png'}
+                  width={20}
+                  height={20}
+                  alt='pfp'
+                />
+                <p className='font-medium text-gray-300'>Placeholder</p>
+              </div>
+              <Tags tag1={'Adventure'} tag2={'Drama'} tag3={'Dark Fantasy'} />
+            </footer>
+          </article>
+        </div>
+      </Link>
+    </article>
+  )
+}
