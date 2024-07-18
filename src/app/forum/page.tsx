c7a73b338f062e9032df6b81cddffe86f8f55b9f
Yerovy Espitia <luyeesgar@gmail.com>
2024-07-18 14:26:54 -0500

    feat: add PostCards to forum page

--- src/app/forum/page.tsx
+++ src/app/forum/page.tsx
@@ -1,9 +1,14 @@
+import { PostCards } from '@/components/Cards/post'
 import { Container } from '@/components/Container'
 
 export default function Forum() {
   return (
     <Container>
-      <h1 className='mt-6'>forum...</h1>
+      <PostCards title={'Recently posted'} />
+      <PostCards title={'Recent activity'} />
+      <PostCards title={'Top trending posts'} />
+      <PostCards title={'Most like posts'} />
+      <PostCards title={'Bug reports'} />
     </Container>
   )
 }
