12b591e8f2c8f0b1e8c2bde5e09264d0ba10c72a
Yerovy Espitia <luyeesgar@gmail.com>
2024-07-18 14:29:53 -0500

    style: change div tag to article + p to Link

--- src/components/Cards/column.tsx
+++ src/components/Cards/column.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { ColumnCard } from '../Card/column'
 
 interface PropsCards {
@@ -7,10 +8,15 @@ interface PropsCards {
 export const ColumnCards = ({ title }: PropsCards) => {
   return (
     <section className='mt-6 mb-16'>
-      <div className='flex items-center justify-between mb-3'>
+      <article className='flex items-center justify-between mb-3'>
         <h2 className='text-lg font-semibold text-gray-300'>{title}</h2>
-        <p className='font-medium text-gray-200 text-md'>View all</p>
-      </div>
+        <Link
+          href={'/game'}
+          className='font-medium text-gray-200 text-md hover:underline'
+        >
+          View all
+        </Link>
+      </article>
 
       <section className='flex flex-col gap-3'>
         {Array.from({ length: 4 }).map((_, index) => (
