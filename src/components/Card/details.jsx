cacedf0c7c743d5b858e0581ce63600cc2267de6
Yerovy Espitia <luyeesgar@gmail.com>
2024-07-17 19:32:13 -0500

    feat: add titles

--- src/components/Card/details.jsx
+++ src/components/Card/details.jsx
@@ -2,36 +2,42 @@ import Image from 'next/image'
 
 export const GameDetails = () => {
   return (
-    <div className='flex items-start justify-between w-full gap-3 lg:w-auto lg:gap-14'>
-      <div className='flex items-center gap-2'>
-        <span>😀</span>
-        <p className='text-sm font-semibold text-gray-500'>50%</p>
+    <div className='flex items-start justify-between w-full gap-3 lg:w-auto lg:gap-12'>
+      <div className='flex flex-col gap-1'>
+        <p className='text-gray-400 font-semibold text-[14px]'>Rating</p>
+        <div className='flex items-center gap-2'>
+          <span>😀</span>
+          <p className='text-sm font-semibold text-gray-500'>50%</p>
+        </div>
       </div>
 
-      <div className='flex items-center gap-2'>
-        <span>💬</span>
-        <p className='text-sm font-semibold text-gray-500'>2448</p>
+      <div className='flex flex-col gap-1'>
+        <p className='text-gray-400 font-semibold text-[14px]'>Reviews</p>
+        <div className='flex items-center gap-2'>
+          <span>💬</span>
+          <p className='text-sm font-semibold text-gray-500'>2448</p>
+        </div>
       </div>
 
-      <div className='flex flex-row gap-2 lg:flex-col'>
-        {/* <p className='order-2 text-sm font-semibold text-gray-500 lg:order-1'>
-          Platforms
-        </p> */}
-        <div className='flex gap-2'>
-          <Image
-            className='size-4'
-            src={'/svgs/ps.svg'}
-            width={16}
-            height={16}
-            alt='playstation'
-          />
-          <Image
-            className='size-4'
-            src={'/svgs/xbox.svg'}
-            width={16}
-            height={16}
-            alt='xbox'
-          />
+      <div className='flex flex-col gap-1'>
+        <p className='text-gray-400 font-semibold text-[14px]'>Platforms</p>
+        <div className='flex flex-row gap-2 lg:flex-col'>
+          <div className='flex gap-2'>
+            <Image
+              className='size-4'
+              src={'/svgs/ps.svg'}
+              width={16}
+              height={16}
+              alt='playstation'
+            />
+            <Image
+              className='size-4'
+              src={'/svgs/xbox.svg'}
+              width={16}
+              height={16}
+              alt='xbox'
+            />
+          </div>
         </div>
       </div>
     </div>
