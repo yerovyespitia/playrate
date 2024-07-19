b0838cd7f5f3f9e52d358c9fb75e97c517c4ebc7
Yerovy Espitia <luyeesgar@gmail.com>
2024-07-18 19:11:38 -0500

    refactor: change Tags props to string array + change breakpoints + alignments

--- src/components/Card/column.tsx
+++ src/components/Card/column.tsx
@@ -7,14 +7,14 @@ export const ColumnCard = () => {
     <article className='border rounded-lg border-tertiary hover:bg-secondary'>
       <Link href={'/game'}>
         <div className='flex flex-col items-start justify-between gap-3 p-3 lg:gap-0 lg:items-center lg:flex-row'>
-          <div className='flex flex-col items-center sm:flex-row'>
+          <div className='flex flex-col items-start sm:flex-row'>
             <img
               className='mb-3 sm:mb-0 sm:w-[120px] sm:h-[170px] lg:h-[160px] object-cover rounded-lg'
               src={'/imgs/square.png'}
               alt='square'
             />
 
-            <article className='flex flex-col gap-3 sm:pl-3 sm:max-w-[470px]'>
+            <article className='flex flex-col gap-3 sm:pl-3 lg:max-w-[470px]'>
               <h2 className='text-lg font-semibold text-gray-400 capitalize'>
                 The last of us
               </h2>
@@ -25,7 +25,7 @@ export const ColumnCard = () => {
                 doloribus.
               </p>
 
-              <Tags tag1={'Adventure'} tag2={'Drama'} tag3={'Dark Fantasy'} />
+              <Tags tags={['Adventure', 'Drama', 'Dark Fantasy']} />
 
               <section className='inline lg:hidden'>
                 <GameDetails />
