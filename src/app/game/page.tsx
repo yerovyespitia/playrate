33d3b867c5f88a4d08941b99167998bbd83a182f
Yerovy Espitia <luyeesgar@gmail.com>
2024-07-30 14:32:22 -0500

    feat: add new comps to game route

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,9 +1,22 @@
 import { Container } from '@/components/Container'
+// import { Comments } from '@/components/Game/comments'
+import { Details } from '@/components/Game/detailts'
+import { Header } from '@/components/Game/header'
 
 export default function Game() {
   return (
-    <Container>
-      <h1 className='mt-6'>game...</h1>
-    </Container>
+    <>
+      <Container>
+        <section className='flex flex-col gap-4'>
+          <Header />
+          <Details />
+          {/* <section className='flex flex-col gap-3'>
+            {Array.from({ length: 4 }).map((_, index) => (
+              <Comments key={index} />
+            ))}
+          </section> */}
+        </section>
+      </Container>
+    </>
   )
 }
