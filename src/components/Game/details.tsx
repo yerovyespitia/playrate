0d9e27ca39e3fe58760a7a4a3ac6e9b4c1b6ce29
Yerovy Espitia <luyeesgar@gmail.com>
2024-08-28 14:53:16 -0500

    feat: add details comp

---
+++ src/components/Game/details.tsx
@@ -0,0 +1,18 @@
+export const Details = () => {
+  return (
+    <article>
+      <h2 className='mb-2 text-lg font-semibold text-zinc-400'>
+        The Last of Us
+      </h2>
+      <p className='text-gray-200 leading-7'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+        cupiditate unde laborum, modi optio provident neque commodi perferendis!
+        Sint facilis cum quidem voluptates fugit, voluptate earum illum! Quia,
+        eaque debitis? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Alias cupiditate unde laborum, modi optio provident neque commodi
+        perferendis! Sint facilis cum quidem voluptates fugit, voluptate earum
+        illum! Quia, eaque debitis?
+      </p>
+    </article>
+  )
+}
