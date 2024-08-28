ef9ea7ac442f14ab9ed01a7a4ee66b3dfd3adff7
Yerovy Espitia <luyeesgar@gmail.com>
2024-08-28 14:52:10 -0500

    feat: add tabs comp

---
+++ src/components/Game/tabs.tsx
@@ -0,0 +1,10 @@
+export const Tabs = () => {
+  return (
+    <ul className='text-white flex gap-5 bg-secondary py-4 pl-5 rounded-lg'>
+      <li>Description</li>
+      <li>Reviews</li>
+      <li>Stats</li>
+      <li>Info</li>
+    </ul>
+  )
+}
