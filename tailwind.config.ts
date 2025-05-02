0a305e0291e885541e2a0f9e9b55847306a5b286
Yerovy Espitia <luyeesgar@gmail.com>
2025-05-02 08:49:23 -0500

    chore: update dependencies and configuration for Tailwind CSS and TypeScript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,11 +20,6 @@ const config: Config = {
         '1xl': '1440px',
         '2xl': '1520px',
       },
-      colors: {
-        primary: '#1E1E1E',
-        secondary: '#252525',
-        tertiary: '#404040',
-      },
       backgroundImage: {
         'game-header': "url('/imgs/square.png')",
       },
